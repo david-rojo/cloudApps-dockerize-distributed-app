ce1210027bed3e01aa11d5eaafe26a18c9ea83f5
david-rojo <davidrojotech@gmail.com>
2021-03-20 01:48:13 +0100

    add weatherservice Buildpacks

--- build-publish-docker-images.sh
+++ build-publish-docker-images.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo '##################'
+echo '##################'
 echo '## EOLO PLANNER ##'
+echo '##################'
 echo '##################'
 echo ''
 
@@ -9,13 +11,14 @@ echo 'Script started'
 echo ''
 
 # server
-echo 'SERVER'
-echo '------'
-echo 'Building server image...'
+echo ' --------'
+echo '| SERVER |'
+echo ' --------'
 cd server
+echo 'Building server image...'
 docker build -f wait-for-it.Dockerfile -t drojo/eoloplanner-server .
-cd ..
 echo 'Server image built'
+cd ..
 docker tag drojo/eoloplanner-server drojo/eoloplanner-server:1.0
 echo 'Pushing server images to dockerhub'
 docker push drojo/eoloplanner-server
@@ -24,13 +27,14 @@ echo 'Server images published in dockerhub'
 echo ''
 
 # planner
-echo 'PLANNER'
-echo '-------'
-echo 'Building planner image...'
+echo ' ---------'
+echo '| PLANNER |'
+echo ' ---------'
 cd planner
+echo 'Building planner image...'
 docker build -f cache-multistage.Dockerfile -t drojo/eoloplanner-planner .
-cd ..
 echo 'Planner image built'
+cd ..
 docker tag drojo/eoloplanner-planner drojo/eoloplanner-planner:1.0
 echo 'Pushing planner images to dockerhub'
 docker push drojo/eoloplanner-planner
@@ -39,15 +43,32 @@ echo 'Planner images published in dockerhub'
 echo ''
 
 # toposervice
-echo 'TOPOSERVICE'
-echo '-----------'
-echo 'Building toposervice image...'
+echo ' -------------'
+echo '| TOPOSERVICE |'
+echo ' -------------'
 cd toposervice
+echo 'Building toposervice image...'
 mvn compile jib:build -Dimage=drojo/eoloplanner-toposervice
-echo 'Toposervice image built and published in dockerhub using JIB'
+# is specified in toposervice/pom.xml that versions latest and 1.0 will be built and published
+echo 'Toposervice images built and published in dockerhub using JIB'
 cd ..
 echo ''
 
+# weatherservice
+echo ' ----------------'
+echo '| WEATHERSERVICE |'
+echo ' ----------------'
+cd weatherservice
+echo 'Building weatherservice image...'
+pack build drojo/eoloplanner-weatherservice --path . --builder gcr.io/buildpacks/builder:v1
+echo 'Weatherservice image built using Buildpacks'
+cd ..
+docker tag drojo/eoloplanner-weatherservice drojo/eoloplanner-weatherservice:1.0
+echo 'Pushing weatherservice images to dockerhub'
+docker push drojo/eoloplanner-weatherservice
+docker push drojo/eoloplanner-weatherservice:1.0
+echo 'Weatherservice images published in dockerhub'
 echo ''
+
 echo 'Script execution successfully finished!'
 echo ''
