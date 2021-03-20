957a69d2131b8758552acf9ea7fdaefa71eb087e
david-rojo <davidrojotech@gmail.com>
2021-03-21 00:53:26 +0100

    use var for dockerhub user

--- build-publish-docker-images.sh
+++ build-publish-docker-images.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DOCKERHUB_NAME=drojo
+
 echo '##################'
 echo '##################'
 echo '## EOLO PLANNER ##'
@@ -12,19 +14,22 @@ echo ''
 
 # Build and publish docker image versions latest and 1.0 for each service
 
+echo 'dockerhub user: '$DOCKERHUB_NAME
+echo ''
+
 # server
 echo ' --------'
 echo '| SERVER |'
 echo ' --------'
 cd server
 echo 'Building server image...'
-docker build -f wait-for-it.Dockerfile -t drojo/eoloplanner-server .
+docker build -f wait-for-it.Dockerfile -t $DOCKERHUB_NAME/eoloplanner-server .
 echo 'Server image built'
 cd ..
-docker tag drojo/eoloplanner-server drojo/eoloplanner-server:1.0
+docker tag $DOCKERHUB_NAME/eoloplanner-server $DOCKERHUB_NAME/eoloplanner-server:1.0
 echo 'Pushing server images to dockerhub'
-docker push drojo/eoloplanner-server
-docker push drojo/eoloplanner-server:1.0
+docker push $DOCKERHUB_NAME/eoloplanner-server
+docker push $DOCKERHUB_NAME/eoloplanner-server:1.0
 echo 'Server images published in dockerhub'
 echo ''
 
@@ -34,13 +39,13 @@ echo '| PLANNER |'
 echo ' ---------'
 cd planner
 echo 'Building planner image...'
-docker build -f cache-multistage.Dockerfile -t drojo/eoloplanner-planner .
+docker build -f cache-multistage.Dockerfile -t $DOCKERHUB_NAME/eoloplanner-planner .
 echo 'Planner image built'
 cd ..
-docker tag drojo/eoloplanner-planner drojo/eoloplanner-planner:1.0
+docker tag $DOCKERHUB_NAME/eoloplanner-planner $DOCKERHUB_NAME/eoloplanner-planner:1.0
 echo 'Pushing planner images to dockerhub'
-docker push drojo/eoloplanner-planner
-docker push drojo/eoloplanner-planner:1.0
+docker push $DOCKERHUB_NAME/eoloplanner-planner
+docker push $DOCKERHUB_NAME/eoloplanner-planner:1.0
 echo 'Planner images published in dockerhub'
 echo ''
 
@@ -50,7 +55,7 @@ echo '| TOPOSERVICE |'
 echo ' -------------'
 cd toposervice
 echo 'Building toposervice image...'
-mvn compile jib:build -Dimage=drojo/eoloplanner-toposervice
+mvn compile jib:build -Dimage=$DOCKERHUB_NAME/eoloplanner-toposervice
 # is specified in toposervice/pom.xml that versions latest and 1.0 will be built and published
 echo 'Toposervice images built and published in dockerhub using JIB'
 cd ..
@@ -62,13 +67,13 @@ echo '| WEATHERSERVICE |'
 echo ' ----------------'
 cd weatherservice
 echo 'Building weatherservice image...'
-pack build drojo/eoloplanner-weatherservice --path . --builder gcr.io/buildpacks/builder:v1
+pack build $DOCKERHUB_NAME/eoloplanner-weatherservice --path . --builder gcr.io/buildpacks/builder:v1
 echo 'Weatherservice image built using Buildpacks'
 cd ..
-docker tag drojo/eoloplanner-weatherservice drojo/eoloplanner-weatherservice:1.0
+docker tag $DOCKERHUB_NAME/eoloplanner-weatherservice $DOCKERHUB_NAME/eoloplanner-weatherservice:1.0
 echo 'Pushing weatherservice images to dockerhub'
-docker push drojo/eoloplanner-weatherservice
-docker push drojo/eoloplanner-weatherservice:1.0
+docker push $DOCKERHUB_NAME/eoloplanner-weatherservice
+docker push $DOCKERHUB_NAME/eoloplanner-weatherservice:1.0
 echo 'Weatherservice images published in dockerhub'
 echo ''
 
--- server/wait-for-it.Dockerfile
+++ server/wait-for-it.Dockerfile
@@ -1,21 +1,7 @@
-######################################
-# Base image for compiling container #
-######################################
-FROM node:lts-alpine as builder
-
-# Define our work dir as /usr/src/app/
-WORKDIR /usr/src/app/
-
-# Copy dependency file
-COPY package.json /usr/src/app/
-
-# Install only needed dependencies for the application
-RUN npm install --only=production
-
 ########################################
 # Base image for application container #
 ########################################
-FROM node:14
+FROM node:14.16
 
 # Specify this variable for right execution of libraries in production mode
 ENV NODE_ENV production
@@ -23,18 +9,18 @@ ENV NODE_ENV production
 # Define our work dir as /usr/src/app/
 WORKDIR /usr/src/app/
 
-# install needed software
-RUN apt-get update && apt-get install -y curl
-
 # Download wait-for-it.sh script
 RUN curl -LJO https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh \
     && chmod +x /usr/src/app/wait-for-it.sh
 
-# Copy node_modules folder with all installed dependencies in the compiling image
-COPY --from=builder /usr/src/app/node_modules /usr/src/app/node_modules
-
 # Copy application files
+COPY package.json /usr/src/app/
 COPY src /usr/src/app/src
+COPY public /usr/src/app/public
+COPY config /usr/src/app/config
+
+# Install only needed dependencies for the application
+RUN npm install --only=production
 
 # Indicate exposed port by container
 EXPOSE 3000
